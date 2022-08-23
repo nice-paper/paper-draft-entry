9eabd566076dce32a194323ae751a8aeb52f3db1
awmleer <awmleer@icloud.com>
2022-08-23 18:02:38 +0800

    .

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import importHTML from "import-html-entry";
 
 // TODO
-importHTML("http://127.0.0.1:8080/index.html").then((res) => {
+importHTML(
+  "https://paper-draft-site.oss-cn-hangzhou.aliyuncs.com/index.html"
+).then((res) => {
   console.log(res.template);
   console.log(res.assetPublicPath);
   window.assetPublicPath = res.assetPublicPath;
